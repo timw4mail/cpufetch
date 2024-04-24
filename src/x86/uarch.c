a6cef1d4ca72b117d8119ed06afcee5c7bb33d63
Timothy J. Warren <tim@timshomepage.net>
2024-04-24 16:50:41 -0400

    Add Zhaoxin WuDaoKou

--- src/x86/uarch.c
+++ src/x86/uarch.c
@@ -122,6 +122,7 @@ enum {
   UARCH_MP6_SHRINK,
   UARCH_ESTHER,
   UARCH_ISAIAH,
+  UARCH_WUDAOKOU,
   UARCH_LUJIAZUI
 };
 
@@ -415,9 +416,10 @@ struct uarch* get_uarch_from_cpuid_other(uint32_t ef, uint32_t f, uint32_t em, u
   //                 EF  F  EM   M   S                                             //
   UARCH_START
   CHECK_UARCH(arch,  0,  5,  0,  0,  NA, "mP6",        UARCH_MP6,          250) // sandpile.org
-  CHECK_UARCH(arch,  0,  5,  0,  2,  NA, "iDragon",    UARCH_MP6_SHRINK,   180) // sandpile.org
+  CHECK_UARCH(arch,  0,  5,  0,  2,  NA, "mP6",        UARCH_MP6_SHRINK,   180) // sandpile.org
   CHECK_UARCH(arch,  0,  6,  0, 10,  NA, "Esther",     UARCH_ESTHER,        90)
   CHECK_UARCH(arch,  0,  6,  0, 15,  10, "Isaiah",     UARCH_ISAIAH,        65)
+  CHECK_UARCH(arch,  0,  7,  1, 11,   0, "WuDaoKou",   UARCH_WUDAOKOU,      28)
   CHECK_UARCH(arch,  0,  7,  3, 11,   0, "LuJiaZui",   UARCH_LUJIAZUI,      16)
   UARCH_END
 
